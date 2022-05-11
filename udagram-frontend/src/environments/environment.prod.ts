aa22993c515eeade743a8528bbaf4957a2909bad
Michael Milad <koko94936@gmail.com>
2022-05-11 16:17:33 +0200

    added /api/v0

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-server-dev.eba-smiivgf2.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://udagram-server-dev.eba-smiivgf2.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
